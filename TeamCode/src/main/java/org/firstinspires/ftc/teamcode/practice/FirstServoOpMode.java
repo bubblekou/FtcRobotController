60d6f8e8cba2d90f845d1bdbf40cd59b01a3062a
Hongbing Kou <hongbing@uber.com>
2020-11-04 16:05:33 -0800

    Added more examples

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practice/FirstServoOpMode.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.practice;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="17099: Servo OpMode", group="Linear Opmode")
+//@Disabled
+public class FirstServoOpMode extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private Servo firstServo = null;
+    private String servoName = "first_servo";
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        firstServo = hardwareMap.get(Servo.class, servoName);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            // check to see if we need to move the servo.
+            if(gamepad1.y) {
+                // move to 0 degrees.
+                firstServo.setPosition(0);
+            } else if (gamepad1.x || gamepad1.b) {
+                // move to 90 degrees.
+                firstServo.setPosition(0.5);
+            } else if (gamepad1.a) {
+                // move to 180 degrees.
+                firstServo.setPosition(1);
+            }
+            telemetry.addData("Servo Position", firstServo.getPosition());
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+        }
+    }
+}
