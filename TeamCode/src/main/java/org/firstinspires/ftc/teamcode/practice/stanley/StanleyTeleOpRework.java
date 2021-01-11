5121c0b9176b68d588f0ebeb043f3dc54483f89c
DuckLordIII <stanleyliu975@gmail.com>
2021-01-10 20:30:20 -0800

    made it more reliable by turning down the confidence

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practice/stanley/StanleyTeleOpRework.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practice/stanley/StanleyTeleOpRework.java
@@ -130,25 +130,11 @@ public class StanleyTeleOpRework extends LinearOpMode {
         waitForStart();
 
         if (opModeIsActive()) {
+
             while (opModeIsActive()) {
-                if (tfod != null) {
-                    // getUpdatedRecognitions() will return null if no new information is available since
-                    // the last time that call was made.
-                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                    if (updatedRecognitions != null) {
-                        telemetry.addData("# Object Detected", updatedRecognitions.size());
-                        // step through the list of recognitions and display boundary info.
-                        int i = 0;
-                        for (Recognition recognition : updatedRecognitions) {
-                            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                                    recognition.getLeft(), recognition.getTop());
-                            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                                    recognition.getRight(), recognition.getBottom());
-                        }
-                        telemetry.update();
-                    }
-                }
+                telemetry.addData("rings", ringAmount());
+                telemetry.update();
+                sleep (100);
             }
         }
 
@@ -182,7 +168,7 @@ public class StanleyTeleOpRework extends LinearOpMode {
         int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
                 "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minResultConfidence = 0.8f;
+        tfodParameters.minResultConfidence = 0.6f;
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
         tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
     }
