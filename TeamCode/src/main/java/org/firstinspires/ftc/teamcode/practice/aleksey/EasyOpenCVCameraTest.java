f6a76923556768744d1ef2130186915200d5ad67
RampageRager <alekseyvalouev@gmail.com>
2021-01-16 10:24:47 -0800

    EasyOpenCVCameraTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practice/aleksey/EasyOpenCVCameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practice/aleksey/EasyOpenCVCameraTest.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc.teamcode.practice.aleksey;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
