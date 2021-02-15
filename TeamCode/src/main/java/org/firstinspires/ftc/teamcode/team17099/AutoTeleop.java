96718c121427c271de913dea3fadcbe8cb966587
Hongbing Kou <hongbing@uber.com>
2021-02-14 21:33:28 -0800

    refactor ring amount to int type

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team17099/AutoTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team17099/AutoTeleop.java
@@ -42,15 +42,15 @@ public class AutoTeleop extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         //import the team bot so we have access to all the stuff in it.
         this.bot = new GyroDriveRobot(hardwareMap, this);
-        while (!isStarted()) {
-            sleep(100);
-            idle();
-        }
 
-        double maxRings = 0;
-        double ringAmt;
+        telemetry.addData(">", "Press Play to start op mode");
+        // Wait for the game to start (driver presses PLAY)
+        telemetry.update();
+        waitForStart();
+
+        int maxRings = 0;
         for (int i = 0; i < 10; i++){
-            ringAmt = bot.getRingAmount();
+            int ringAmt = bot.getRingAmount();
             if (ringAmt > maxRings){
                 maxRings = ringAmt;
             }
@@ -58,14 +58,9 @@ public class AutoTeleop extends LinearOpMode {
         }
         bot.shutdownTfod();
 
-        telemetry.addData(">", "Press Play to start op mode");
-        // Wait for the game to start (driver presses PLAY)
-        telemetry.update();
-
-        waitForStart();
         if (maxRings == 0){
             gotoTargetA();
-        }else if (maxRings == 1){
+        } else if (maxRings == 1){
             gotoTargetB();
         }else{
             gotoTargetC();
