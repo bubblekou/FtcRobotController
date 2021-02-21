126bd92206c4aacb041f35ca044737f20f521d60
Ethan Kou <bluecheetos666@gmail.com>
2021-02-21 13:08:41 -0800

    made new EthanDoubleWobble file for 2 wobbles in auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practice/ethan/EthanDoubleWobble.java
@@ -0,0 +1,120 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.practice.ethan;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.team17099.GyroDriveRobot;
+
+/**
+ * This file illustrates the concept of driving a path based on Gyro heading and encoder counts.
+ * It uses the common Pushbot hardware class to define the drive on the robot.
+ * The code is structured as a LinearOpMode
+ *
+ * The code REQUIRES that you DO have encoders on the wheels,
+ *   otherwise you would use: PushbotAutoDriveByTime;
+ *
+ *  This code ALSO requires that you have a Modern Robotics I2C gyro with the name "gyro"
+ *   otherwise you would use: PushbotAutoDriveByEncoder;
+ *
+ *  This code requires that the drive Motors have been configured such that a positive
+ *  power command moves them forward, and causes the encoders to count UP.
+ *
+ *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
+ *
+ *  In order to calibrate the Gyro correctly, the robot must remain stationary during calibration.
+ *  This is performed when the INIT button is pressed on the Driver Station.
+ *  This code assumes that the robot is stationary when the INIT button is pressed.
+ *  If this is not the case, then the INIT should be performed again.
+ *
+ *  Note: in this example, all angles are referenced to the initial coordinate frame set during the
+ *  the Gyro Calibration process, or whenever the program issues a resetZAxisIntegrator() call on the Gyro.
+ *
+ *  The angle of movement/rotation is assumed to be a standardized rotation around the robot Z axis,
+ *  which means that a Positive rotation is Counter Clock Wise, looking down on the field.
+ *  This is consistent with the FTC field coordinate conventions set out in the document:
+ *  ftc_app\doc\tutorial\FTC_FieldCoordinateSystemDefinition.pdf
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="EthanDoubleWobble", group="Ethan's Autonomous")
+//@Disabled
+public class EthanDoubleWobble extends LinearOpMode {
+    private GyroDriveRobot bot;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        /*
+         * Initialize the standard drive system variables.
+         * The init() method of the hardware class does most of the work here
+         */
+        this.bot = new GyroDriveRobot(hardwareMap, this);
+
+        while (!isStarted()) {
+            sleep(100);
+            idle();
+        }
+
+        bot.liftArm();
+
+        bot.gyroDrive(0.5, -54, 0);
+        bot.gyroTurn(0.2, 90);
+        bot.gyroHold(0.2, 90, 0.2);
+        bot.gyroTurn(0.2, 180);
+        bot.gyroHold(0.2, 180, 0.2);
+
+        int count = 0;
+        bot.shootRing(0.9);
+        while (opModeIsActive() && count < 3) {
+            count++;
+            sleep(1000);
+            telemetry.addData(">", "Ring " + count);
+            telemetry.update();
+            bot.pushRing();
+        }
+        bot.stopFlywheel();
+
+        bot.gyroTurn(0.3, -90);
+        bot.gyroDrive(0.5, -16, 0);
+        bot.dropArm();
+        sleep(200);
+        bot.flipGrabber();
+        sleep(200);
+        bot.liftArm();
+
+        bot.gyroDrive(0.5, 10, 0);
+        bot.gyroTurn(0.3, 180);
+        bot.gyroDrive(0.3, -12, 0);
+    }
+}
